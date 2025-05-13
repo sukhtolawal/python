c40262bc5832168d19817d02b9c1db7adf6fd702
sukhtolawal <sukhwindersidhu81@gmail.com>
2025-05-13 22:52:41 +0530

    inital commit

---
+++ venv/main.py
@@ -0,0 +1,19 @@
+from flask import Flask,request
+
+from flask_cors import CORS  # type: ignore
+
+# Create Flask app and SocketIO instance
+app = Flask(__name__)
+CORS(app)
+
+
+
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    # Run the Flask app
+    app.run()
